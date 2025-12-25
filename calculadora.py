26873dcd7a1bfd45cf125b4985cfb263107c1825
Jeamir Quispe <jeamirquispe20@gmail.com>
2025-12-24 22:11:15 -0500

    Cambios de las 10

---
+++ calculadora.py
@@ -0,0 +1,4 @@
+print("Funciones de suma ")
+print("Funciones de resta ")
+
+print("Aca va otras cosas pero bueno caad cambio q se hace en nuna rama q es difernete a l aprin cipal se tiene q agregar al staged y una vez eso se realiza el commit es en resu,men como guardar tus cambios en la rama en la q estas trabajando ")
